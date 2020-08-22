eae79d8693606305717c2aa38229a00e44efb71f
zhaobozhen <zhaobozhen2025@gmail.com>
2020-08-22 18:05:35 +0800

    Create utils library.

---
+++ utils/src/main/java/com/absinthe/libraries/utils/AntiShakeUtils.kt
@@ -0,0 +1,40 @@
+package com.absinthe.libraries.utils
+
+import android.view.View
+import androidx.annotation.IntRange
+import com.absinthe.libraries.R
+
+/**
+ * Anti-Shake click utils
+ */
+object AntiShakeUtils {
+    private const val INTERNAL_TIME: Long = 1000
+
+    /**
+     * Whether this click event is invalid.
+     *
+     * @param target       target view
+     * @param internalTime the internal time. The unit is millisecond.
+     * @return true, invalid click event.
+     */
+    fun isInvalidClick(
+            target: View,
+            @IntRange(from = 0) internalTime: Long = INTERNAL_TIME
+    ): Boolean {
+        val curTimeStamp = System.currentTimeMillis()
+        val o = target.getTag(R.id.last_click_time)
+
+        if (o == null) {
+            target.setTag(R.id.last_click_time, curTimeStamp)
+            return false
+        }
+
+        val lastClickTimeStamp = o as Long
+        val isInvalid = curTimeStamp - lastClickTimeStamp < internalTime
+
+        if (!isInvalid) {
+            target.setTag(R.id.last_click_time, curTimeStamp)
+        }
+        return isInvalid
+    }
+}
